8d0fdc4f5ff4d1d166a02b2076226f4f06c58b5b
WillianGGjava <willianggvet@gmail.com>
2022-01-14 17:44:12 -0300

    Corrigir html

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 const imagem = document.querySelector('img');
-const botao = document.querySelector('button');
+const botao = document.querySelector('#button');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie=document.querySelector('#especie');
+const condicao=documento.querySelector('#status');
+
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2` , {
+        method:'GET',
+        headers:{
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.especies;
+        condicao.innerHTML = data.status;
+    });
+}
+
+botao.onclick = pegarPersonagem;
