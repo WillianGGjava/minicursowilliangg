46f2d9628548d90e5024c17dfd3b900ff134d796
WillianGGjava <willianggvet@gmail.com>
2022-01-14 20:36:18 -0300

    Criar consumo de api  Rickand Morty

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 const imagem = document.querySelector('img');
-const botao = document.querySelector('#button');
+const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
-const especie=document.querySelector('#especie');
-const condicao=documento.querySelector('#status');
+const especie= document.querySelector('#especie');
+const condicao = document.querySelector('#status');
 
 gerarValorAleatorio = () => {
-    return Math.florr(Math.random() * 671);
+    return Math.floor(Math.random() * 671);
 }
 
 pegarPersonagem = () => {
     let numeroAleatorio = gerarValorAleatorio();
-    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}` , {
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
         method:'GET',
         headers:{
             Accept: 'application/json',
-            "Content-type": 'application/json'
+            "Content-type":'application/json'
         }
     }).then((response) => response.json()).then((data) => {
         imagem.src = data.image;
         imagem.alt = data.name;
         nomeDoPersonagem.innerHTML = data.name;
-        especie.innerHTML = data.especies;
+        especie.innerHTML = data.species;
         condicao.innerHTML = data.status;
     });
 }
