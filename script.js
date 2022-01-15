40f5eee01ff3b529e0f82436b7e1ac4701842577
WillianGGjava <willianggvet@gmail.com>
2022-01-15 12:18:38 -0300

    Implementar nome,especie,status,respectivos de cada imagem

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const imagem = document.body.querySelectorAll('img');
 const botao = document.querySelector('button');
-const nomeDoPersonagem = document.querySelector('#nome');
-const especie= document.querySelector('#especie');
-const condicao = document.querySelector('#status');
+const listaDeNumeros = [0,1,2]
 
 gerarValorAleatorio = () => {
     return Math.floor(Math.random() * 671);
 }
 
 pegarPersonagem = () => {
-    imagem.forEach(img =>{
+    listaDeNumeros.forEach(elemento =>{
     let numeroAleatorio = gerarValorAleatorio();
     return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
         method:'GET',
@@ -18,9 +16,11 @@ pegarPersonagem = () => {
             "Content-type":'application/json'
         }
     }).then((response) => response.json()).then((data) => {
-        
-            img.src = data.image;
-            img.alt = data.name;
+            var nomeDoPersonagem = document.querySelector('#nome' + elemento.toString());
+            var especie= document.querySelector('#especie' + elemento.toString());
+            var condicao = document.querySelector('#status' + elemento.toString());
+            imagem[elemento].src = data.image;
+            imagem[elemento].alt = data.name;
             nomeDoPersonagem.innerHTML = data.name;
             especie.innerHTML = data.species;
             condicao.innerHTML = data.status;})
